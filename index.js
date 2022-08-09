7b051ea2e0f8ebce11b80b0d5d6faac4e2b61cf2
Megha Bodke <megha.n.bodke@gmail.com>
2022-08-09 11:49:25 +0530

    cleanup for consoles and alert

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-const bookTitle = document.getElementById("title");
-const author = document.getElementById("author");
-const submitbtn = document.getElementById("submitbtn");
-const displayTitle = document.getElementById("displayTitle");
-const displayAuthor = document.getElementById("displayAuthor");
-const remove = document.getElementById("removebtn");
-const bookName = document.getElementById("bookName");
+const bookTitle = document.getElementById('title');
+const author = document.getElementById('author');
+const submitbtn = document.getElementById('submitbtn');
+const displayTitle = document.getElementById('displayTitle');
+const displayAuthor = document.getElementById('displayAuthor');
+const remove = document.getElementById('removebtn');
+const bookName = document.getElementById('bookName');
 
 /* Book add functionality */
-submitbtn.addEventListener("click", () => {
+submitbtn.addEventListener('click', () => {
   let books = [];
-  if (localStorage.getItem("booksInformation")) {
-    alert("in if block");
-    books = JSON.parse(localStorage.getItem("booksInformation"));
-    console.log(books);
+  if (localStorage.getItem('booksInformation')) {
+    books = JSON.parse(localStorage.getItem('booksInformation'));
     books.push({ title: bookTitle.value, author: author.value });
     displayTitle.innerText = bookTitle.value;
     displayTitle.innerText = displayAuthor.value;
   } else {
-    alert("something wrong");
+    books.push({ title: bookTitle.value, author: author.value });
+    displayTitle.innerText = bookTitle.value;
+    displayTitle.innerText = displayAuthor.value;
+    /* alert('something wrong'); */
   }
-  localStorage.setItem("booksInformation", JSON.stringify(books));
+  localStorage.setItem('booksInformation', JSON.stringify(books));
 });
 
 /* Remove book functionality */
-remove.addEventListener("click", () => {
-  alert("remove clicked");
-  books = JSON.parse(localStorage.getItem("booksInformation"));
-  console.log(books);
+remove.addEventListener('click', () => {
+  books = JSON.parse(localStorage.getItem('booksInformation'));
+  /* console.log(books); */
   books = books.splice(bookName.value, 1);
-  localStorage.setItem("booksInformation", JSON.stringify(books));
+  localStorage.setItem('booksInformation', JSON.stringify(books));
   /* console.log(books); */
 });
