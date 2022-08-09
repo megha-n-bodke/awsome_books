64e67c99d80bbd443a20ee08417fee38940bac10
Megha Bodke <megha.n.bodke@gmail.com>
2022-08-09 09:39:43 +0530

    markup changes to add remove functionality

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const bookTitle = document.getElementById("title");
 const author = document.getElementById("author");
 const submitbtn = document.getElementById("submitbtn");
+const displayTitle = document.getElementById("displayTitle");
+const displayAuthor = document.getElementById("displayAuthor");
+const remove = document.getElementById("removebtn");
 
+/* Book add functionality */
 submitbtn.addEventListener("click", () => {
   let books = [];
-
   if (localStorage.getItem("booksInformation")) {
     alert("in if block");
     books = JSON.parse(localStorage.getItem("booksInformation"));
     console.log(books);
     books.push({ title: bookTitle.value, author: author.value });
+    displayTitle.innerText = bookTitle.value;
+    displayTitle.innerText = displayAuthor.value;
   } else {
     alert("something wrong");
   }
-
   localStorage.setItem("booksInformation", JSON.stringify(books));
+});
+
+/* Remove book functionality */
+remove.addEventListener("click", () => {
+  alert("remove clicked");
 });
