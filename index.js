d9ef1ca989815ea8b61f6f7aac5a00906954cdd8
Megha Bodke <megha.n.bodke@gmail.com>
2022-08-12 18:44:34 +0530

    add display and remove book functionality

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const bookTitle = document.getElementById("title");
 const author = document.getElementById("author");
 const submitbtn = document.getElementById("submitbtn");
 const displayDiv = document.getElementById("display");
-
-/* display data */
+const errorMsg = document.getElementById("emptySpan");
 let contentDiv = "";
+
+/* const displayDiv = document.getElementById("display");
+ */
+/* display data */
+/* let contentDiv = "";
 let books = [];
 document.addEventListener("DOMContentLoaded", () => {
   const display = () => {
@@ -58,3 +62,84 @@ document.addEventListener("DOMContentLoaded", () => {
     display();
   };
 });
+ */
+
+/* class declaration for book */
+let books = [];
+class SingleBook {
+  constructor(name, author) {
+    this.name = name;
+    this.author = author;
+  }
+
+  /* Book add functionality */
+  addBook() {
+    let bookName = bookTitle.value;
+    let authorName = author.value;
+    const book = new SingleBook(bookName, authorName);
+
+    /* store in localstorage */
+    if (localStorage.getItem("booksInfo") === null) {
+      books.push(book);
+      localStorage.setItem("booksInfo", JSON.stringify(books));
+    } else {
+      books = JSON.parse(localStorage.getItem("booksInfo"));
+      books.push(book);
+      localStorage.setItem("booksInfo", JSON.stringify(books));
+    }
+
+    contentDiv = "";
+    alert("data submitted successfully");
+    bookTitle.value = "";
+    author.value = "";
+    displayDiv.innerHTML = contentDiv;
+    this.display();
+  }
+
+  /* display  localstorage data */
+  display() {
+    if (localStorage.getItem("booksInfo")) {
+      books = JSON.parse(localStorage.getItem("booksInfo"));
+    } else {
+      alert("in else block");
+      localStorage.setItem("booksInfo", JSON.stringify(books));
+    }
+
+    books.map((singlebook, index) => {
+      contentDiv += `
+                <p>${singlebook.name} by ${singlebook.author}</p>
+                <button id="btn" onclick='removeDiv(${index})'>Remove</button>
+              `;
+
+      return contentDiv;
+    });
+    displayDiv.innerHTML = contentDiv;
+  }
+
+  /* remove book functionality */
+  remove(id) {
+    books = JSON.parse(localStorage.getItem("booksInfo"));
+    books.splice(id, 1);
+    localStorage.setItem("booksInfo", JSON.stringify(books));
+    contentDiv = "";
+    displayDiv.innerHTML = contentDiv;
+    this.display();
+  }
+}
+
+/* on page load display data of local storage */
+document.addEventListener("DOMContentLoaded", () => {
+  const book = new SingleBook();
+  book.display();
+});
+
+/* add book functionality */
+submitbtn.addEventListener("click", () => {
+  const book = new SingleBook();
+  book.addBook();
+});
+
+removeDiv = (id) => {
+  const book = new SingleBook();
+  book.remove(id);
+};
