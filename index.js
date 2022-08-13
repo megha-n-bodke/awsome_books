f7575a92ac04aff4c5ac057eae3a43ea2b7951d6
Megha Bodke <megha.n.bodke@gmail.com>
2022-08-13 15:09:58 +0530

    corrected linter errors

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const contact = document.getElementById("contact-details");
-const form = document.getElementById("books-form");
-const booksDetails = document.getElementById("book-display");
-const booklink = document.getElementById("bookList");
-const formlink = document.getElementById("addBook");
-const contactLink = document.getElementById("contact");
+const contact = document.getElementById('contact-details');
+const form = document.getElementById('books-form');
+const booksDetails = document.getElementById('book-display');
+const booklink = document.getElementById('bookList');
+const formlink = document.getElementById('addBook');
+const contactLink = document.getElementById('contact');
 
 /* remove book functionality */
 
-const bookTitle = document.getElementById("title");
-const author = document.getElementById("author");
-const submitbtn = document.getElementById("submitbtn");
-const displayDiv = document.getElementById("display");
-const errorMsg = document.getElementById("emptySpan");
-let contentDiv = "";
+const bookTitle = document.getElementById('title');
+const author = document.getElementById('author');
+const submitbtn = document.getElementById('submitbtn');
+const displayDiv = document.getElementById('display');
+const errorMsg = document.getElementById('emptySpan');
+let contentDiv = '';
 
 /* class declaration for book */
 let books = [];
@@ -29,31 +29,31 @@ class SingleBook {
     const book = new SingleBook(bookName, authorName);
 
     /* store in localstorage */
-    if (bookTitle.value === "" || author.value === "") {
-      errorMsg.innerText = "please enter data";
-    } else if (localStorage.getItem("booksInfo") === null) {
+    if (bookTitle.value === '' || author.value === '') {
+      errorMsg.innerText = 'please enter data';
+    } else if (localStorage.getItem('booksInfo') === null) {
       books.push(book);
-      localStorage.setItem("booksInfo", JSON.stringify(books));
+      localStorage.setItem('booksInfo', JSON.stringify(books));
     } else {
-      books = JSON.parse(localStorage.getItem("booksInfo"));
+      books = JSON.parse(localStorage.getItem('booksInfo'));
       books.push(book);
-      localStorage.setItem("booksInfo", JSON.stringify(books));
+      localStorage.setItem('booksInfo', JSON.stringify(books));
     }
 
-    contentDiv = "";
-    bookTitle.value = "";
-    author.value = "";
+    contentDiv = '';
+    bookTitle.value = '';
+    author.value = '';
     displayDiv.innerHTML = contentDiv;
     this.display();
   }
 
   /* display  localstorage data */
   display() {
-    if (localStorage.getItem("booksInfo")) {
-      books = JSON.parse(localStorage.getItem("booksInfo"));
+    if (localStorage.getItem('booksInfo')) {
+      books = JSON.parse(localStorage.getItem('booksInfo'));
     } else {
-      alert("in else block");
-      localStorage.setItem("booksInfo", JSON.stringify(books));
+      alert('in else block');
+      localStorage.setItem('booksInfo', JSON.stringify(books));
     }
 
     books.map((singlebook, index) => {
@@ -66,58 +66,58 @@ class SingleBook {
 
       return contentDiv;
     });
-    booklink.style.color = "blue";
+    booklink.style.color = 'blue';
     displayDiv.innerHTML = contentDiv;
   }
 
   remove(id) {
-    books = JSON.parse(localStorage.getItem("booksInfo"));
+    books = JSON.parse(localStorage.getItem('booksInfo'));
     books.splice(id, 1);
-    localStorage.setItem("booksInfo", JSON.stringify(books));
-    contentDiv = "";
+    localStorage.setItem('booksInfo', JSON.stringify(books));
+    contentDiv = '';
     displayDiv.innerHTML = contentDiv;
     this.display();
   }
 
   /* display books */
   displayBooks() {
-    booksDetails.classList.add("show-books");
-    booksDetails.classList.remove("remove-books");
-    form.classList.remove("show-form");
-    contact.classList.remove("show-contact");
-    booklink.style.color = "blue";
-    formlink.style.color = "black";
-    contactLink.style.color = "black";
+    booksDetails.classList.add('show-books');
+    booksDetails.classList.remove('remove-books');
+    form.classList.remove('show-form');
+    contact.classList.remove('show-contact');
+    booklink.style.color = 'blue';
+    formlink.style.color = 'black';
+    contactLink.style.color = 'black';
   }
 
   displayForm() {
-    booksDetails.classList.remove("show-books");
-    booksDetails.classList.add("remove-books");
-    form.classList.add("show-form");
-    contact.classList.remove("show-contact");
-    booklink.style.color = "black";
-    formlink.style.color = "blue";
-    contactLink.style.color = "black";
+    booksDetails.classList.remove('show-books');
+    booksDetails.classList.add('remove-books');
+    form.classList.add('show-form');
+    contact.classList.remove('show-contact');
+    booklink.style.color = 'black';
+    formlink.style.color = 'blue';
+    contactLink.style.color = 'black';
   }
 
   displayContact() {
-    booksDetails.classList.remove("show-books");
-    booksDetails.classList.add("remove-books");
-    form.classList.remove("show-form");
-    contact.classList.add("show-contact");
-    booklink.style.color = "black";
-    formlink.style.color = "black";
-    contactLink.style.color = "blue";
+    booksDetails.classList.remove('show-books');
+    booksDetails.classList.add('remove-books');
+    form.classList.remove('show-form');
+    contact.classList.add('show-contact');
+    booklink.style.color = 'black';
+    formlink.style.color = 'black';
+    contactLink.style.color = 'blue';
   }
 }
 
 /* on page load display data of local storage */
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   const book = new SingleBook();
   book.display();
 
   /* add book functionality */
-  submitbtn.addEventListener("click", () => {
+  submitbtn.addEventListener('click', () => {
     const book = new SingleBook();
     book.addBook();
   });
@@ -127,17 +127,17 @@ document.addEventListener("DOMContentLoaded", () => {
     book.remove(id);
   };
 
-  booklink.addEventListener("click", () => {
+  booklink.addEventListener('click', () => {
     const book = new SingleBook();
     book.displayBooks();
   });
 
-  formlink.addEventListener("click", () => {
+  formlink.addEventListener('click', () => {
     const book = new SingleBook();
     book.displayForm();
   });
 
-  contactLink.addEventListener("click", () => {
+  contactLink.addEventListener('click', () => {
     const book = new SingleBook();
     book.displayContact();
   });
